dc23e95147b24af279ec7e7dda98f21a55cbf9fb
Joseph Niet <josephniet@gmail.com>
2014-03-30 22:05:30 +0100

    jslinted

--- angular/js/app.js
+++ angular/js/app.js
@@ -2,7 +2,7 @@
 
      // Declare app level module which depends on filters, and services
     angular.module('myApp', [
-      'ngSanitize',
+      'ngSanitize'
     ]).run(function($document, $rootScope, $q, $window){
         var tag = $document[0].createElement('script');
         tag.src = "https://www.youtube.com/iframe_api";
@@ -15,23 +15,23 @@
         };
     })
     .controller('MainCtrl', ['$scope', '$rootScope', '$http', 'APIKey', 'data', 'parentID', '$timeout', function($scope, $rootScope, $http, APIKey, data, parentID, $timeout) {
-        function _notifyParent(data){
-            window.top.acfYoutubeRead(parentID, data, $('#myApp').height() )
-        };
+        function notifyParent(data){
+            window.top.acfYoutubeRead(parentID, data, $('#myApp').height() );
+        }
         $scope.r = data || false;
         $scope.$watch('r', function(val){
            // if (!val) return;
             $timeout(function(){
-                _notifyParent(val)
-            },500)  
-        })        
+                notifyParent(val);
+            }, 500);
+        });      
         
          $scope.fetch = function(id){
             $rootScope.youtubeReady.promise.then(function(){
                 var baseURL = 'https://www.googleapis.com/youtube/v3',
                     resource = '/videos?',
-                    parts = 'part=' + encodeURIComponent( 'id,contentDetails,player,snippet' );
-                var url = baseURL + resource + parts + '&id=' + id + '&key=' + APIKey;
+                    parts = 'part=' + encodeURIComponent( 'id,contentDetails,player,snippet' ),
+                    url = baseURL + resource + parts + '&id=' + id + '&key=' + APIKey;
                 $http({method: 'GET', url: url}).success(function(data, status, headers, config) {
                     //console.log('data!', data);
                     $scope.r = data.items[0];
@@ -40,7 +40,7 @@
                   // called asynchronously if an error occurs
                   // or server returns response with an error status.
                 });
-            })
-          }
-    }])
+            });
+          };
+    }]);
 
